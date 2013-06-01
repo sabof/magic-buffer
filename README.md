3975b18051c676636675a43473558674794740d6
sabof <esabof@gmail.com>
2013-06-01 20:03:11 +0100

    packaging

---
+++ README.md
@@ -0,0 +1,16 @@
+# magic-buffer
+An executable cookbook, on how to (ab)use emacse's display engine. Feel free to add sections, or suggest improvements.
+
+## Trans-network autoload
+You can add the following snippet to your .emacs. It will download and install the latest version of magic-buffer on it's firsts run.
+
+    (defun magic-buffer ()
+      (interactive)
+      (with-current-buffer
+          (url-retrieve-synchronously
+           "https://raw.github.com/sabof/magic-buffer/master/magic-buffer.el")
+        (goto-char (point-min))
+        (search-forward "\n\n")
+        (delete-region (point-min) (point))
+        (eval-buffer))
+      (magic-buffer))
--- magic-buffer.el
+++ magic-buffer.el
@@ -1,4 +1,34 @@
-;; -*- truncate-lines: nil; lexical-binding: t-*-
+;;; magic-buffer.el --- -*- lexical-binding: t-*-
+;;; Version: 0.1
+;;; Author: sabof
+;;; URL: https://github.com/sabof/magic-buffer
+
+;;; Commentary:
+
+;; The project is hosted at https://github.com/sabof/magic-buffer
+;; The latest version, and all the relevant information can be found there.
+
+;;; License:
+
+;; This file is NOT part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program ; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Code:
+
 (require 'cl-lib)
 (defvar mb-sections nil)
 (setq mb-sections nil)
@@ -21,7 +51,7 @@
 
 (mb-section "Horizontal line"
   "The point-entered property prevents the point from staying on that location,\
- since that would change the color of the line."
+since that would change the color of the line. Under"
   (insert (propertize
            ;; (concat (make-string 999 ?\s ) "\n")
            "\n"
